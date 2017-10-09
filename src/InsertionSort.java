4472ab7fbe98e5a7240e8208035503fbbb394caa
neohanju <neohanju@gmail.com>
2017-10-10 01:18:48 +0900

    implement shell sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,11 +1,12 @@
-package edu.princeton.cs.algs4;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
 
 public class InsertionSort {
 	public static void sort(Comparable[] a) {
 		int N = a.length;
 		for (int i = 1; i < N; i++)
-			for (int j = i; j > 1; j--)
-				if (less(a[j], a[j-1])) exch(a, j, j-1);
+			for (int j = i; j > 0 && less(a[j], a[j-1]); j--)
+				exch(a, j, j-1);
 	}
 	
 	private static boolean less(Comparable v, Comparable w) {
